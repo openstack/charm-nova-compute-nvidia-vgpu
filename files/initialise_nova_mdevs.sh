da993a07f4631dcf2743ea732558cc95788d4b14
Edward Hope-Morley <edward.hope-morley@canonical.com>
2025-09-26 22:14:18 +0100

    Fix racey mdev init workaround
    The original implementation is prone to racing with
    the nvidia driver so that mdevs are not registered by
    the time the script runs.
    
    Change-Id: I0dd8002a91d4026a71b8176610efac28ea50c33b
    Signed-off-by: Edward Hope-Morley <edward.hope-morley@canonical.com>
    (cherry picked from commit 1b992b26e67074ac8bd585c1ad748b9b733cb7a4)

--- files/initialise_nova_mdevs.sh
+++ files/initialise_nova_mdevs.sh
@@ -1,6 +1,16 @@
 #!/bin/bash -e
 # Ensure all SRIOV devices have been setup
-/usr/lib/nvidia/sriov-manage -e srvio-manage -e ALL
+sleep 20
+/usr/lib/nvidia/sriov-manage -e ALL
+# Ensure mdev devices are registered before continuing
+max=10
+while true; do
+    if ! $(/usr/lib/nvidia/sriov-manage -e  $(nvidia-smi -q | grep ^GPU| cut -d ' ' -f2-)| grep -q "already has VFs enabled."); then
+        echo "Waiting for GPU nvidia mdev registration"
+        sleep 1
+        ((max--)) && continue
+    fi
+    break
+done
 # Now go through all domains and initialise any used mdevs
 /opt/remediate-nova-mdevs
-
